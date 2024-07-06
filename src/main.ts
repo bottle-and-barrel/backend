c270df87317eb288c92640ce972d0d3488cb1287
Dmitry Evteev <haxrelt@gmail.com>
2024-07-06 20:34:45 +0300

    enable CORS

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.enableCors({
+    methods: '*',
+    origin: process.env.CORS_ORIGIN ?? 'localhost:3000',
+    credentials: true,
+  });
 
   const config = new DocumentBuilder().addBearerAuth().build();
   const document = SwaggerModule.createDocument(app, config);
